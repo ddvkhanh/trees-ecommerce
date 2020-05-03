5042e380d9d1788851c2bc472d770419a3c19787
thaiquocdang <56788585+thaiquocdang@users.noreply.github.com>
2020-05-03 18:31:44 +1000

    delete unnecessary function

--- src/Shop.js
+++ src/Shop.js
@@ -74,23 +74,26 @@ export default function Shop(props) {
               <div className="shop-sorting-data d-flex flex-wrap align-items-center justify-content-between">
                 {/* Shop Page Count */}
                 <div className="shop-page-count">
-                  <p>Showing 1–9 of 72 results</p>
+                  <p>Showing 1–{totalCount} of {totalCount} results</p>
                 </div>
                 {/* Search by Terms */}
                 <div className="search_by_terms">
                   <form action="#" method="post" className="form-inline">
                     <select className="custom-select widget-title">
                       <option selected>Short by Popularity</option>
-                      <option value={1}>Short by Newest</option>
-                      <option value={2}>Short by Sales</option>
-                      <option value={3}>Short by Ratings</option>
+                      <option value={1}>Short by A-Z</option>
+                      <option value={2}>Short by Z-A</option>
+                      <option value={3}>Short by price low to high</option>
+                      <option value={4}>Short by price high to low</option>
                     </select>
-                    <select className="custom-select widget-title">
+                    {/* 
+                      <select className="custom-select widget-title">
                       <option selected>Show: 9</option>
                       <option value={1}>12</option>
                       <option value={2}>18</option>
                       <option value={3}>24</option>
-                    </select>
+                      </select>
+                    */}
                   </form>
                 </div>
               </div>
@@ -100,20 +103,21 @@ export default function Shop(props) {
             {/* Sidebar Area */}
             <div className="col-12 col-md-4 col-lg-3">
               <div className="shop-sidebar-area">
-                {/* Shop Widget */}
-                <div className="shop-widget price mb-50">
+                {/* Shop Widget 
+                  <div className="shop-widget price mb-50">
                   <h4 className="widget-title">Prices</h4>
                   <div className="widget-desc">
-                    <div className="slider-range">
-                      <div data-min={8} data-max={30} data-unit="$" className="slider-range-price ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all" data-value-min={8} data-value-max={30} data-label-result="Price:">
-                        <div className="ui-slider-range ui-widget-header ui-corner-all" />
-                        <span className="ui-slider-handle ui-state-default ui-corner-all first-handle" tabIndex={0} />
-                        <span className="ui-slider-handle ui-state-default ui-corner-all" tabIndex={0} />
-                      </div>
-                      <div className="range-price">Price: $8 - $30</div>
-                    </div>
+                  <div className="slider-range">
+                  <div data-min={8} data-max={30} data-unit="$" className="slider-range-price ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all" data-value-min={8} data-value-max={30} data-label-result="Price:">
+                  <div className="ui-slider-range ui-widget-header ui-corner-all" />
+                  <span className="ui-slider-handle ui-state-default ui-corner-all first-handle" tabIndex={0} />
+                  <span className="ui-slider-handle ui-state-default ui-corner-all" tabIndex={0} />
                   </div>
-                </div>
+                  <div className="range-price">Price: $8 - $30</div>
+                  </div>
+                  </div>
+                  </div>
+                */}
                 {/* Shop Widget */}
                 <div className="shop-widget catagory mb-50">
                   <h4 className="widget-title">Categories</h4>
@@ -138,93 +142,96 @@ export default function Shop(props) {
                   </div>
                 </div>
                 {/* Shop Widget */}
-                <div className="shop-widget sort-by mb-50">
+                {/*
+                  <div className="shop-widget sort-by mb-50">
                   <h4 className="widget-title">Sort by</h4>
                   <div className="widget-desc">
-                    {/* Single Checkbox */}
-                    <div className="custom-control custom-checkbox d-flex align-items-center mb-2">
-                      <input type="checkbox" className="custom-control-input" id="customCheck7" />
-                      <label className="custom-control-label" htmlFor="customCheck7">New arrivals</label>
-                    </div>
-                    {/* Single Checkbox */}
-                    <div className="custom-control custom-checkbox d-flex align-items-center mb-2">
-                      <input type="checkbox" className="custom-control-input" id="customCheck8" />
-                      <label className="custom-control-label" htmlFor="customCheck8">Alphabetically, A-Z</label>
-                    </div>
-                    {/* Single Checkbox */}
-                    <div className="custom-control custom-checkbox d-flex align-items-center mb-2">
-                      <input type="checkbox" className="custom-control-input" id="customCheck9" />
-                      <label className="custom-control-label" htmlFor="customCheck9">Alphabetically, Z-A</label>
-                    </div>
-                    {/* Single Checkbox */}
-                    <div className="custom-control custom-checkbox d-flex align-items-center mb-2">
-                      <input type="checkbox" className="custom-control-input" id="customCheck10" />
-                      <label className="custom-control-label" htmlFor="customCheck10">Price: low to high</label>
-                    </div>
-                    {/* Single Checkbox */}
-                    <div className="custom-control custom-checkbox d-flex align-items-center">
-                      <input type="checkbox" className="custom-control-input" id="customCheck11" />
-                      <label className="custom-control-label" htmlFor="customCheck11">Price: high to low</label>
-                    </div>
+                
+                  <div className="custom-control custom-checkbox d-flex align-items-center mb-2">
+                  <input type="checkbox" className="custom-control-input" id="customCheck7" />
+                  <label className="custom-control-label" htmlFor="customCheck7">New arrivals</label>
                   </div>
-                </div>
-                {/* Shop Widget */}
-                <div className="shop-widget best-seller mb-50">
+                  
+                  <div className="custom-control custom-checkbox d-flex align-items-center mb-2">
+                  <input type="checkbox" className="custom-control-input" id="customCheck8" />
+                  <label className="custom-control-label" htmlFor="customCheck8">Alphabetically, A-Z</label>
+                  </div>
+                  
+                  <div className="custom-control custom-checkbox d-flex align-items-center mb-2">
+                  <input type="checkbox" className="custom-control-input" id="customCheck9" />
+                  <label className="custom-control-label" htmlFor="customCheck9">Alphabetically, Z-A</label>
+                  </div>
+                  
+                  <div className="custom-control custom-checkbox d-flex align-items-center mb-2">
+                  <input type="checkbox" className="custom-control-input" id="customCheck10" />
+                  <label className="custom-control-label" htmlFor="customCheck10">Price: low to high</label>
+                  </div>
+                  
+                  <div className="custom-control custom-checkbox d-flex align-items-center">
+                  <input type="checkbox" className="custom-control-input" id="customCheck11" />
+                  <label className="custom-control-label" htmlFor="customCheck11">Price: high to low</label>
+                  </div>
+                  </div>
+                  </div>
+                */}
+                {/* Shop Widget 
+                  <div className="shop-widget best-seller mb-50">
                   <h4 className="widget-title">Best Seller</h4>
                   <div className="widget-desc">
-                    {/* Single Best Seller Products */}
-                    <div className="single-best-seller-product d-flex align-items-center">
-                      <div className="product-thumbnail">
-                        <a href="shop-details.html"><img src="img/bg-img/4.jpg" alt="" /></a>
-                      </div>
-                      <div className="product-info">
-                        <a href="shop-details.html">Cactus Flower</a>
-                        <p>$10.99</p>
-                        <div className="ratings">
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                        </div>
-                      </div>
-                    </div>
-                    {/* Single Best Seller Products */}
-                    <div className="single-best-seller-product d-flex align-items-center">
-                      <div className="product-thumbnail">
-                        <a href="shop-details.html"><img src="img/bg-img/5.jpg" alt="" /></a>
-                      </div>
-                      <div className="product-info">
-                        <a href="shop-details.html">Tulip Flower</a>
-                        <p>$11.99</p>
-                        <div className="ratings">
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                        </div>
-                      </div>
-                    </div>
-                    {/* Single Best Seller Products */}
-                    <div className="single-best-seller-product d-flex align-items-center">
-                      <div className="product-thumbnail">
-                        <a href="shop-details.html"><img src="img/bg-img/34.jpg" alt="" /></a>
-                      </div>
-                      <div className="product-info">
-                        <a href="shop-details.html">Recuerdos Plant</a>
-                        <p>$9.99</p>
-                        <div className="ratings">
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                        </div>
-                      </div>
-                    </div>
+                  
+                  <div className="single-best-seller-product d-flex align-items-center">
+                  <div className="product-thumbnail">
+                  <a href="shop-details.html"><img src="img/bg-img/4.jpg" alt="" /></a>
                   </div>
-                </div>
+                  <div className="product-info">
+                  <a href="shop-details.html">Cactus Flower</a>
+                  <p>$10.99</p>
+                  <div className="ratings">
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  </div>
+                  </div>
+                  </div>
+                  
+                  <div className="single-best-seller-product d-flex align-items-center">
+                  <div className="product-thumbnail">
+                  <a href="shop-details.html"><img src="img/bg-img/5.jpg" alt="" /></a>
+                  </div>
+                  <div className="product-info">
+                  <a href="shop-details.html">Tulip Flower</a>
+                  <p>$11.99</p>
+                  <div className="ratings">
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  </div>
+                  </div>
+                  </div>
+                  
+                  <div className="single-best-seller-product d-flex align-items-center">
+                  <div className="product-thumbnail">
+                  <a href="shop-details.html"><img src="img/bg-img/34.jpg" alt="" /></a>
+                  </div>
+                  <div className="product-info">
+                  <a href="shop-details.html">Recuerdos Plant</a>
+                  <p>$9.99</p>
+                  <div className="ratings">
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  <i className="fa fa-star" />
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                */}
               </div>
             </div>
             {/* All Products Area */}
@@ -238,14 +245,15 @@ export default function Shop(props) {
                   ))}
 
                 </div>
-                {/* Pagination */}
-                <nav aria-label="Page navigation">
+                {/* Pagination 
+                  <nav aria-label="Page navigation">
                   <ul className="pagination">
-                    <li className="page-item"><a className="page-link" href="#">1</a></li>
-                    <li className="page-item"><a className="page-link" href="#">2</a></li>
-                    <li className="page-item"><a className="page-link" href="#"><i className="fa fa-angle-right" /></a></li>
+                  <li className="page-item"><a className="page-link" href="#">1</a></li>
+                  <li className="page-item"><a className="page-link" href="#">2</a></li>
+                  <li className="page-item"><a className="page-link" href="#"><i className="fa fa-angle-right" /></a></li>
                   </ul>
-                </nav>
+                  </nav>
+                */}
               </div>
             </div>
           </div>
