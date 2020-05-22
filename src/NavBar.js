025dddf036547dd35e241e0dea779a0723f9740d
Khanh Dang <dangdovankhanh@gmail.com>
2020-05-22 22:21:06 +1000

    no message

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,104 +1,75 @@
 import React, { Component } from "react";
-import {PRODUCT_URL} from "./components/Products/Products"
+import { PRODUCT_URL } from "./components/Products/Products";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+
 export default class NavBar extends Component {
-    render() {
-        return (
-            <div className="alazea-main-menu">
-                <div className="classy-nav-container breakpoint-off">
-                    <div className="container">
-                        {/* <!-- Menu --> */}
-                        <nav className="classy-navbar justify-content-between" id="alazeaNav">
-                            {/* <!-- Nav Brand --> */}
-                            <a href="index.html" className="nav-brand">
-                                <img id="header-logo" src="img/core-img/Logo03.png" alt="" />
-                            </a>
-                            {/* <!-- Navbar Toggler --> */}
-                            <div className="classy-navbar-toggler">
-                                <span className="navbarToggler">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </span>
-                            </div>
-                            {/* <!-- Menu --> */}
-                            <div className="classy-menu"></div>
-                            {/* <!-- Close Button --> */}
-                            <div className="classycloseIcon">
-                                <div className="cross-wrap">
-                                    <span className="top"></span>
-                                    <span className="bottom"></span>
-                                </div>
-                            </div>
-                            {/* <!-- Navbar Start --> */}
-                            <div className="classynav">
-                                <ul>
-                                    <li>
-                                        <a href="/">Home</a>
-                                    </li>
-                                    <li>
-                                        <a href="/about">About</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Pages</a>
-                                        <ul className="dropdown">
-                                            <li>
-                                                <a href="/">Home</a>
-                                            </li>
-                                            <li>
-                                                <a href="/about">About</a>
-                                            </li>
-                                            <li>
-                                                <a href={PRODUCT_URL}>Shop</a>
-                                                <ul className="dropdown">
-                                                    <li>
-                                                        <a href={PRODUCT_URL}>Shop</a>
-                                                    </li>
-                                                    
-                                                    <li>
-                                                        <a href="cart.html">Shopping Cart</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="checkout.html">Checkout</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="portfolio.html">Portfolio</a>
-                                                <ul className="dropdown">
-                                                    <li>
-                                                        <a href="portfolio.html">Portfolio</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="single-portfolio.html">Portfolio Details</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                      
-                                            <li>
-                                                <a href="contact.html">Contact</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href={PRODUCT_URL}>Shop</a>
-                                    </li>
-                                    <li>
-                                        <a href="portfolio.html">Portfolio</a>
-                                    </li>
-                                    <li>
-                                        <a href="/contact">Contact</a>
-                                    </li>
-                                </ul>
-                                {/* <!-- Search Icon --> */}
-                                <div id="searchIcon">
-                                    <i className="fa fa-search" aria-hidden="true"></i>
-                                </div>
-                            </div>
-                            {/* <!-- Navbar End --> */}
-                        </nav>
-                    </div>
+  render() {
+    return (
+      <div className="alazea-main-menu">
+        <div className="classy-nav-container breakpoint-off">
+          <div className="container">
+            {/* <!-- Menu --> */}
+            <nav
+              className="classy-navbar justify-content-between"
+              id="alazeaNav"
+            >
+              {/* <!-- Nav Brand --> */}
+              <a href="index.html" className="nav-brand">
+                <img id="header-logo" src="img/core-img/Logo03.png" alt="" />
+              </a>
+              {/* <!-- Navbar Toggler --> */}
+              <div className="classy-navbar-toggler">
+                <span className="navbarToggler">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </div>
+              {/* <!-- Menu --> */}
+              <div className="classy-menu"></div>
+              {/* <!-- Close Button --> */}
+              <div className="classycloseIcon">
+                <div className="cross-wrap">
+                  <span className="top"></span>
+                  <span className="bottom"></span>
                 </div>
-            </div>
-        );
-    }
+              </div>
+              {/* <!-- Navbar Start --> */}
+              <div className="classynav">
+                <Router>
+                  <div>
+                    <ul>
+                      <li>
+                        <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                        <Link to="/about">About</Link>
+                      </li>
+                      <li>
+                        <Link to={PRODUCT_URL}>Shop</Link>
+                      </li>
+                      <li>
+                        <Link to="/portfolio">Portfolio</Link>
+                      </li>
+                      <li>
+                        <Link to="/contact">Contact</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </Router>
+              </div>
+              {/* <!-- Navbar End --> */}
+            </nav>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
